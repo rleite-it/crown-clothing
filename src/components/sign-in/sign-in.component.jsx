524a888d10ec3d492cfcb7a227be810987456fa2
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2022-02-28 17:40:16 +0000

    Update sign-in with google button type

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -54,7 +54,7 @@ const SignIn = (props) => {
 				/>
 				<div className="buttons">
 					<CustomButton type="submit">Sign In</CustomButton>
-					<CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+					<CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>
 						Sign in with Google
 					</CustomButton>
 				</div>
